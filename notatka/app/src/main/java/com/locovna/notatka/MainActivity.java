8d8291055ca8663cf2d2dc7d2021a10825a26035
locovna <locovna@gmail.com>
2017-01-17 12:49:35 +0200

    implement recycle view with hardcoded content

--- notatka/app/src/main/java/com/locovna/notatka/MainActivity.java
+++ notatka/app/src/main/java/com/locovna/notatka/MainActivity.java
@@ -1,7 +1,10 @@
 package com.locovna.notatka;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    ArrayList<Note> notes = new ArrayList<Note>();
+    notes.add(new Note("First notatka", "first blood"));
+    notes.add(new Note("Second notatka", "there is opopop"));
+    notes.add(new Note("My Sweet Salmon", "there is opopop, there is opopop, there is opopop, there is opopop"));
+
+    NoteAdapter mNoteAdapter = new NoteAdapter(this, notes);
+
+    ListView listView = (ListView) findViewById(R.id.listview_note);
+    listView.setAdapter(mNoteAdapter);
   }
 }
