0dc239077197587033664e64ec63935f84547997
locovna <locovna@gmail.com>
2017-01-19 13:40:27 +0200

    refactored MainActivity

--- notatka/app/src/main/java/com/locovna/notatka/MainActivity.java
+++ notatka/app/src/main/java/com/locovna/notatka/MainActivity.java
@@ -15,26 +15,28 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
+    
     ArrayList<Note> notes = new ArrayList<Note>();
     notes.add(new Note("First notatka", "first blood"));
     notes.add(new Note("Second notatka", "there is opopop"));
-    notes.add(new Note("My Sweet Salmon", "there is opopop, there is opopop, there is opopop, there is opopop"));
+    notes.add(new Note("My Sweet Salmon", "there is opopop, there is opopop, " +
+        "there is opopop, there is opopop"));
 
     final NoteAdapter mNoteAdapter = new NoteAdapter(this, notes);
-
     ListView listView = (ListView) findViewById(R.id.listview_note);
     listView.setAdapter(mNoteAdapter);
 
+    // Setup each item to open EditorActivity
     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
       public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-        // Find the current earthquake that was clicked on
-        Note currentEarthquake = mNoteAdapter.getItem(position);
+        Note currentNote = mNoteAdapter.getItem(position);
 
         Intent intent = new Intent(MainActivity.this, EditorActivity.class);
         startActivity(intent);
       }
     });
+
+
   }
 }
